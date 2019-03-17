3aeb92153fa7cb68e9d89ea180ee65cd47437575
Михаил Синельников <msin87@yandex.ru>
2019-03-18 00:04:05 +0300

    ...

--- masterclock/IDialog.cpp
+++ masterclock/IDialog.cpp
@@ -5,8 +5,20 @@ IDialog::~IDialog()
 	WM_DeleteWindow(hWin);
 }
 
-void IDialog::createDialogBox()
+void IDialog::createAndShow()
 {
-	
-	hWin = GUI_CreateDialogBox(aDialogCreate, (sizeof(aDialogCreate)/sizeof(aDialogCreate[0])), callBack, hParrent, x0, y0);
+
+	hWin = GUI_CreateDialogBox(aDialogCreate, (sizeof(aDialogCreate) / sizeof(aDialogCreate[0])), callBack, hParrent, x0, y0);
+	hMenuStack.push(hWin);
+}
+
+void IDialog::hide()
+{
+	WM_HideWindow(hWin);
+}
+
+void IDialog::destroy()
+{
+	WM_DeleteWindow(hWin);
+	hWin = 0;
 }
--- masterclock/IDialog.h
+++ masterclock/IDialog.h
@@ -1,18 +1,21 @@
 #pragma once
-#include "functional"
+#include <stack>
 #include "DIALOG.h"
+#include <stdint.h>
 
 class IDialog
 {
 	WM_HWIN hWin;
 	const WM_HWIN hParrent;
-	static WM_HWIN hWinPrev;
-	WM_CALLBACK *callBack;
+	static std::stack<WM_HWIN> hMenuStack;
+	WM_CALLBACK *const callBack;
 	const GUI_WIDGET_CREATE_INFO *aDialogCreate;
 	uint16_t x0, y0;
 
   public:
-	IDialog(const GUI_WIDGET_CREATE_INFO *_aDialogCreate, WM_CALLBACK *_callBack, const WM_HWIN _hParrent, uint16_t _x0, uint16_t _y0) : aDialogCreate(_aDialogCreate), callBack(_callBack), hParrent(_hParrent), x0(_x0), y0(_y0){};
+	IDialog(const GUI_WIDGET_CREATE_INFO *_aDialogCreate, WM_CALLBACK *const _callBack, const WM_HWIN _hParrent, uint16_t _x0, uint16_t _y0) : aDialogCreate(_aDialogCreate), callBack(_callBack), hParrent(_hParrent), x0(_x0), y0(_y0){};
 	~IDialog();
-	void createDialogBox();
+	void createAndShow();
+	void hide();
+	void destroy();
 };
